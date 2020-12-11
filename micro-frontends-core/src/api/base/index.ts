684c3f2a4ee4522f8aae1ada522c9cb297595e36
shikong-sk <shikong-sk@users.noreply.github.com>
2020-12-12 03:10:18 +0800

    Api 层

--- micro-frontends-core/src/api/base/index.ts
+++ micro-frontends-core/src/api/base/index.ts
@@ -1,13 +1,24 @@
 import {AxiosRequestConfig} from "axios";
 import getInstance          from "@/api/instance";
 
+const baseURL: string = "/";
+
 // 请求配置
 const axiosRequestConfig: AxiosRequestConfig = {
-    baseURL: "/",
+    baseURL,
     timeout: 30 * 1000,
 };
 
 // 请求接口
 const instance = getInstance(axiosRequestConfig);
 
+const Api = {
+
+}
+
 export default instance;
+export {
+    baseURL,
+    instance,
+    Api
+}
